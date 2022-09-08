024b44c35b5a82dbf4e490b142a1d953a2f173d5
Aydcoder <aydcoder@gmail.com>
2022-09-08 06:55:54 +0100

    Example Folder

---
+++ example/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+List<Color> colorList = const <Color>[
+  Color(0xff66e0da),
+  Color(0xfff5a2d9),
+  Color(0xfff0c722),
+  Color(0xff6a85e5),
+  Color(0xfffd9a6f),
+  Color(0xff92db6e),
+  Color(0xff73b8e5),
+  Color(0xfffd7590),
+  Color(0xffc78ae5),
+];
